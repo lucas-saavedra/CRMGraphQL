bde22aa96b7cb195dd596bad53d19bf4c5da1ed9
lucas_saavedra <lucassaavedra50@gmail.com>
2022-09-14 09:38:29 -0300

    Added jwt

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { ApolloServer } from "apollo-server";
+import jwt from "jsonwebtoken";
 import { connectDb } from "./config/db.js";
 import { resolvers } from "./db/resolvers.js";
 import { typeDefs } from "./db/schema.js";
+import { SECRET } from "./env.config.js";
 
 
 //db connection
@@ -10,7 +12,19 @@ connectDb();
 
 //server
 const server = new ApolloServer({
-    typeDefs, resolvers
+    typeDefs, resolvers, context: ({ req }) => {
+        const token = req.headers['authorization'] || "";
+        if (token) {
+            try {
+                //Token verification
+                const user = jwt.verify(token, SECRET);
+
+                return { user };
+            } catch (error) {
+                return error
+            }
+        }
+    }
 });
 
 server.listen().then(({ url }) => {
